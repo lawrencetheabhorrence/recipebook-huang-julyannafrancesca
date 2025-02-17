d09a1a991c15712859b352b44baab181591cbc7c
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-02-17 16:51:44 +0800

    added views from ledger into urls

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+import ledger
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("recipes/list", ledger.views.index),
+    path("recipe/<int:recipe_id>", ledger.views.recipe_detail),
 ]
