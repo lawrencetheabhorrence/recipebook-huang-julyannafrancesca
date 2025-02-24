d0e3db20396e4fe6f3d5071cef6519229752a9e2
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-02-24 18:03:12 +0800

    Initial models for recipe and ingredients, project files are gitignored

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=250)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=250)
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.PositiveIntegerField()
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.SET_NULL)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
