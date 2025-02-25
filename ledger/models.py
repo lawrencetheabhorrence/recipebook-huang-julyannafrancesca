05cf3cbe5536f73621090de5ba5ead73f6a77374
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-02-25 10:41:29 +0800

    Added string representation for the Ingredient and Recipe models

--- ledger/models.py
+++ ledger/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
 
 
 class RecipeIngredient(models.Model):
