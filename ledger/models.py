df0bfd5a4b5adc79ce5d8fc9ff44b3fc15b8962f
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-02-25 15:27:05 +0800

    Added absolute_url for the Recipe model

--- ledger/models.py
+++ ledger/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -14,6 +15,9 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("detail", args=[self.pk])
 
 
 class RecipeIngredient(models.Model):
