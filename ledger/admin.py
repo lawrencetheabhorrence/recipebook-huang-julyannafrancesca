7aa17804402cc7de171bc9f5299e0f4dff6f9489
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-02-25 17:12:37 +0800

    migrations + table display and filter for recipeingredient in admin page

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient, RecipeIngredient
 
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ["recipe", "ingredient", "quantity"]
+    list_filter = ["recipe", "ingredient"]
+
+
 # Register your models here.
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
-admin.site.register(RecipeIngredient)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
--- ledger/models.py
+++ ledger/models.py
@@ -4,14 +4,14 @@ from django.urls import reverse
 
 # Create your models here.
 class Ingredient(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250, unique=True)
 
     def __str__(self):
         return self.name
@@ -22,5 +22,13 @@ class Recipe(models.Model):
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=250)
-    ingredient = models.ForeignKey(Ingredient, null=True, on_delete=models.SET_NULL)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(
+        Ingredient,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="ingredient",
+    )
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="recipe")
+
+    def __str__(self):
+        return f"{self.recipe}: {self.ingredient}, {self.quantity}"
