9702b133804739fa08909d80ca4490026992174e
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-03-17 09:14:24 +0800

    Registered user app

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "user.apps.UserConfig",
     "ledger.apps.LedgerConfig",
     "django.contrib.admin",
     "django.contrib.auth",
