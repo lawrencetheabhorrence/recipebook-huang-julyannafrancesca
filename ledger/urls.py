ce8061c3d3550bad692279924469f9c45aca1ffa
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-04-01 15:00:26 +0800

    added in urls for adding ingredients

--- ledger/urls.py
+++ ledger/urls.py
@@ -5,5 +5,11 @@ from ledger import views
 urlpatterns = [
     path("recipes/list", views.index),
     path("recipe/<int:recipe_id>", views.recipe_detail, name="detail"),
+    path(
+        "recipe/<int:recipe_id>/ingredient/add",
+        views.recipeingredient_add,
+        name="recipeingredient_add",
+    ),
+    path("ingredient/add", views.ingredient_add, name="ingredient_add"),
     path("recipe/add", views.recipe_add, name="recipe_add"),
 ]
