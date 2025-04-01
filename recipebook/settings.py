cffa7de9ed30f11d287b8de2f60297b871fa2b9e
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-04-01 16:33:00 +0800

    Moved secret key to env file

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ MEDIA_URL = "/media/"
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rj_*k)j*h2l3nrux@j5wsmm0rg1*t73$1pzal&l5-z3*t^+o=8"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
